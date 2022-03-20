a8cf573a6ef9dc4466501fb952f3b1af2fc47cc4
Sarika Singh <97461783+sarikasingh30@users.noreply.github.com>
2022-03-20 13:55:32 +0530

    Delete ValidatorsAssignment directory

--- ValidatorsAssignment/src/configs/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require("mongoose");
-const connection=()=>{
-    return mongoose.connect("mongodb+srv://sarika:12345@cluster0.3bl32.mongodb.net/Cluster0?authSource=admin&replicaSet=atlas-6x18cj-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
-
-}
-module.exports=connection;
--- ValidatorsAssignment/src/index.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const User=require("./models/user.model");
-const userController=require("./controllers/user.controller");// router is there in controller file
-const app=express();
-app.use(express.json());
-
-
-app.use("/users", userController);
-module.exports=app;
