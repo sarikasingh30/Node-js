a5abc5003fa79d912e3dab77631f7388a9efa6a3
Sarika Singh <97461783+sarikasingh30@users.noreply.github.com>
2022-03-29 20:02:49 +0530

    Update index.js

--- LibrarySystem/index.js
+++ LibrarySystem/index.js
@@ -5,7 +5,7 @@ const mongoose=require("mongoose");
 
 app.use(express.json());
 const connectLib=()=>{
-    return mongoose.connect("mongodb+srv://sarika:12345@cluster0.3bl32.mongodb.net/Cluster0?authSource=admin&replicaSet=atlas-6x18cj-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
+    return mongoose.connect("mongodb+srv://sarika:12345@cluster0.3bl32.mongodb.net/Cluster0?authSource=admin&replicaSet=atlas-6x18cj-shard-0&w=majority");
 }
 
 // USER SCHEMA............................................................................................
