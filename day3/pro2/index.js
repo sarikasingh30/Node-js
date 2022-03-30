70fe5daaa901b1e84d530a7164e1c2235c55b827
Sarika Singh <97461783+sarikasingh30@users.noreply.github.com>
2022-03-30 13:32:19 +0530

    Update index.js

--- day3/pro2/index.js
+++ day3/pro2/index.js
@@ -1,22 +1,22 @@
-// const express=require("express");
-// const app=express();
-// app.use(singleBook);
-// app.get(`/book/:${name}`,function(req,res){
-//      return res.json({book_name: req.name});
-// })
+const express=require("express");
+const app=express();
+app.use(singleBook);
+app.get(`/book/:${name}`,function(req,res){
+     return res.json({book_name: req.name});
+})
 
-// app.get(`/book/:HarryPotter`,function(req,res){
-//     return res.json({book_name: req.name});
-// })
-// function singleBook(req,res,next){
-//     if(req.param.name=="GameofThones"){
-//         req.name="GameofThones"
-//     }
-//     else if(req.param.name=="HarryPotter"){
-//         req.name="HarryPotter"
-//     }
-//     next();
-// }
-// app.listen("3890",()=>{
-//     console.log("Listening to port 3890");
-// })
+app.get(`/book/:HarryPotter`,function(req,res){
+    return res.json({book_name: req.name});
+})
+function singleBook(req,res,next){
+    if(req.param.name=="GameofThones"){
+        req.name="GameofThones"
+    }
+    else if(req.param.name=="HarryPotter"){
+        req.name="HarryPotter"
+    }
+    next();
+}
+app.listen("3890",()=>{
+    console.log("Listening to port 3890");
+})
